204cf69f003f740c2855b716bb2b1a256a9cb7a7
root <guilhermealbuquerquerc@gmail.com>
2017-12-12 15:39:02 -0300

    Tdd

--- src/main.js
+++ src/main.js
@@ -1,2 +1,9 @@
-const soma = (x, y) => x + y;
-soma(1, 2);
+const a = 3;
+const b = 2;
+const c = 'duashssdsadasdasdghdhausdhaushduashduahsdasudhasuhduahsduahsudhasudhaufhauhsuhsfisdfgisgfsdafgsgsghsdsdgh';
+const d = 2;
+
+function sun() {
+  return a + b + c + d;
+}
+ sun();
