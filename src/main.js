f69d83bbb7c35a2f6acb9aaf5fd10144ecf79c02
Guilherme Albuquerque <guilhermealbuquerquerc@gmail.com>
2017-12-13 14:37:17 -0300

    Husky test

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ const a = 3;
 const b = 2;
 const c = 'duashssdsadasdasdghdhausdhaushduashduahsdasudhasuhduahsduahsudhasudhaufhauhsuhsfisdfgisgfsdafgsgsghsdsdgh';
 const d = 2;
+const arr = [1, 2, 3, 4, 5];
+const arrMap = arr.map(i => i !== 0);
+
+console.log(arrMap);
 
 function sun() {
   return a + b + c + d;
 }
- sun();
+
+sun();
